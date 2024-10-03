fd25ee919c4b2bb247a3c3d1b778b595ce386f3d
DoubleMyD <dagostino.dom03@gmail.com>
2024-10-04 00:02:42 +0200

    classes were not utilized

--- app/src/main/java/com/example/lupusinfabulav1/model/voting/CoupleVoteStrategy.kt
+++
@@ -1,4 +0,0 @@
-package com.example.lupusinfabulav1.model.voting
-
-class CoupleVoteStrategy {
-}
--- app/src/main/java/com/example/lupusinfabulav1/model/voting/VotingBehavior.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lupusinfabulav1.model.voting
-
-import com.example.lupusinfabulav1.model.Player
-
-interface VotingBehavior {
-    fun vote(votedPlayers: List<Player>): List<Player>
-}
