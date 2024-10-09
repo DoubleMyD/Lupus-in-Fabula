1eca71c3fad4f81b8bef25935d2a7eaab5b59c1e
DoubleMyD <dagostino.dom03@gmail.com>
2024-10-10 00:17:48 +0200

    renamed from Player to PlayerDetails

--- app/src/main/java/com/example/lupusinfabulav1/ui/VillageUiState.kt
+++ app/src/main/java/com/example/lupusinfabulav1/ui/VillageUiState.kt
@@ -1,7 +1,7 @@
 package com.example.lupusinfabulav1.ui
 
 import com.example.lupusinfabulav1.data.FakePlayersRepository
-import com.example.lupusinfabulav1.model.Player
+import com.example.lupusinfabulav1.model.PlayerDetails
 import com.example.lupusinfabulav1.model.Role
 import com.example.lupusinfabulav1.model.MostVotedPlayer
 import com.example.lupusinfabulav1.model.RoleVotes
@@ -10,10 +10,10 @@ data class VillageUiState(
     val round: Int = 0,
     val gameStarted: Boolean = false,
     val isGameFinished: Boolean = false,
-    val winnerPlayers: List<Player> = emptyList(),
-    val players: List<Player> = FakePlayersRepository.players,
+    val winnerPlayerDetails: List<PlayerDetails> = emptyList(),
+    val playerDetails: List<PlayerDetails> = FakePlayersRepository.playerDetails,
     val currentRole: Role = Role.CITTADINO,
-    val selectedPlayer: Player? = null,
+    val selectedPlayerDetails: PlayerDetails? = null,
     val votedPlayerByRole: Map<Role, MostVotedPlayer> = emptyMap(),   //Per ogni ruolo contiene il giocatore votato nell'ultimo turno del ruolo
     val currentVoting: RoleVotes = RoleVotes(currentRole, emptyList(), emptyList(), emptyList()),
     )
