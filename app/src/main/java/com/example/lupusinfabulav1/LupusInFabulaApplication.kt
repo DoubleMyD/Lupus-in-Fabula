35a62d90cd2006f9f7a891c1e8a2dfb02fb8afda
DoubleMyD <dagostino.dom03@gmail.com>
2024-10-10 00:22:54 +0200

    added context parameter to container

--- app/src/main/java/com/example/lupusinfabulav1/LupusInFabulaApplication.kt
+++ app/src/main/java/com/example/lupusinfabulav1/LupusInFabulaApplication.kt
@@ -8,7 +8,7 @@ class LupusInFabulaApplication : Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = DefaultAppContainer()
+        container = DefaultAppContainer(this)
     }
 
 }
