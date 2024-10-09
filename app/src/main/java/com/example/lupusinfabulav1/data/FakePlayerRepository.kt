0d682e36500e64a18a26154506784e27e222f222
DoubleMyD <dagostino.dom03@gmail.com>
2024-10-10 00:16:47 +0200

    renamed from Player to PlayerDetails

--- app/src/main/java/com/example/lupusinfabulav1/data/FakePlayerRepository.kt
+++ app/src/main/java/com/example/lupusinfabulav1/data/FakePlayerRepository.kt
@@ -1,49 +1,49 @@
 package com.example.lupusinfabulav1.data
 
 import com.example.lupusinfabulav1.R
-import com.example.lupusinfabulav1.model.Player
+import com.example.lupusinfabulav1.model.PlayerDetails
 import com.example.lupusinfabulav1.model.PlayerImageSource
 import com.example.lupusinfabulav1.model.Role
 
 object FakePlayersRepository {
     private var i = 1
-    val players = listOf(
-        Player(
+    val playerDetails = listOf(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.ASSASSINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero1)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CITTADINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero2)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.VEGGENTE,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero3)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.FACILI_COSTUMI,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero4)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.ASSASSINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero5)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CUPIDO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero6)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.MEDIUM,
             imageSource = PlayerImageSource.Resource(R.drawable.ic_launcher_background)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CITTADINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero1)
@@ -70,48 +70,48 @@ object FakePlayersRepository {
 
 
 
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.ASSASSINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero1)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CITTADINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero2)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.VEGGENTE,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero3)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.FACILI_COSTUMI,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero4)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.ASSASSINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero5)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CUPIDO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero6)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.MEDIUM,
             imageSource = PlayerImageSource.Resource(R.drawable.ic_launcher_background)
         ),
-        Player(
+        PlayerDetails(
             name = i++.toString(),
             role = Role.CITTADINO,
             imageSource = PlayerImageSource.Resource(R.drawable.android_superhero1)
         ),
 
-        Player(
+        PlayerDetails(
             name = "fiorellone gigante",
             role = Role.CITTADINO,
             imageSource = PlayerImageSource.Resource(R.drawable.cupido_bow)
